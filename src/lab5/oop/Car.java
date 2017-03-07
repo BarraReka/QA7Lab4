1574324d03e41ab9b81421ddfca0d5c87911e38d
flo <flo.macicasan@gmail.com>
2017-03-07 20:15:31 +0200

    examples with abstract

--- src/lab5/oop/Car.java
+++ src/lab5/oop/Car.java
@@ -7,23 +7,32 @@ package lab5.oop;
 
 public class Car extends Vehicle {
 
-
+    private int speed = 0;
     private String name;
 
     @Override
     public void moves() {
-        System.out.println("Car moves");
+        System.out.println("Car moves with speed " + speed);
         super.stops();
     }
 
     @Override
     public void stops() {
-        System.out.println("Car stops!");
+        System.out.println("Car stops! had speed " + speed);
+    }
+
+    @Override
+    public void accelerate() {
+        speed++;
     }
 
     public static void main(String[] args) {
         Car dacia = new Car();
-
         dacia.moves();
+        dacia.accelerate();
+        dacia.stops();
+
+        Car tanc = new Car();
+        tanc.stops();
     }
 }
--- src/lab5/oop/Vehicle.java
+++ src/lab5/oop/Vehicle.java
@@ -4,7 +4,7 @@ package lab5.oop;
  * @author flo
  * @since 07/03/2017.
  */
-public class Vehicle {
+public abstract class Vehicle  {
 
     public void moves() {
         System.out.println("Vehicle moves!");
@@ -13,6 +13,8 @@ public class Vehicle {
     protected void stops() {
         System.out.println("Vechicle stops!");
     }
+
+    public abstract void accelerate();
 
     public static void main(String[] args) {
         Car dacia = new Car();
